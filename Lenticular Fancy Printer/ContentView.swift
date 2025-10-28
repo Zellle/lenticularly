bcdf27bd6c78b820505c0bf5f36aa6cf6fa728e3
Mathilda <m@mathildasuniverse.com>
2025-10-27 18:18:54 -0700

    Initial Commit

---
+++ Lenticular Fancy Printer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lenticular Fancy Printer
+//
+//  Created by Mathilda on 10/27/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lenticular Fancy Printer/Lenticular_Fancy_PrinterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lenticular_Fancy_PrinterApp.swift
+//  Lenticular Fancy Printer
+//
+//  Created by Mathilda on 10/27/25.
+//
+
+import SwiftUI
+
+@main
+struct Lenticular_Fancy_PrinterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
